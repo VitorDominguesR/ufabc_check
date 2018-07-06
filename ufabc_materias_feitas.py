fd2a6f8c59f59cc99282d92f01f6f89773dbef0d
VitorDominguesR <vitor_dmrb@hotmail.com>
2018-07-06 10:16:11 -0300

    ufabc materias changed regex

--- ufabc_materias_feitas.py
+++ ufabc_materias_feitas.py
@@ -10,9 +10,9 @@ with open("ufabc_materias_eng_info", "r") as materias_enginfo:
         #print(string)
         match = re.split("([0-9]{1,2}\s){4}[0-9]{2}\s", string)
         for item in match:
-            print(item)
+            #print(item)
             try:
-                cod = re.match("\w{4}\d{3}\s-\s\d{2}", item).group(0).strip()
+                cod = re.match("\w{4}\d{3}\s-\s\d{2}", item).group(0).strip().replace(" ","")
                 #print(cod)
                 name_mat = re.sub("\w{4}\d{3}\s-\s\d{2}|((\d{1,2}\s){3}\d)", "", item).strip()
                 #print(name_mat)
@@ -21,8 +21,7 @@ with open("ufabc_materias_eng_info", "r") as materias_enginfo:
                 obrigatorias_file.write("%s;%s\n" % (cod, name_mat))
 
             except Exception as e:
-                print(e)
-
+                pass
             # cod_mat = bytearray(item,'latin1').decode('latin1').split('\n')
             # try:
             #     cod, mat = cod_mat[0]+cod_mat[1]+ cod_mat[2], cod_mat[3:]
